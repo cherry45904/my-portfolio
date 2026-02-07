61c13688c051ea328ad373982ef46ad4e7cb0ef7
cherry <cherry0.xvii@gmail.com>
2026-02-07 16:12:20 +0545

    my first upload

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a,b, c;
+    printf("enter the first number:");
+    scanf("%d",&a);
+    printf("Enter the second number:");
+    scanf("%d",&b);
+    c=a+b;
+    printf("sum=%d",c);
+    getchar();
+    return 0;
+}
+
